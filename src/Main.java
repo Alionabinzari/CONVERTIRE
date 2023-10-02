64caf616cd3e6942c2c3e8e7732d62fd7fcb7317
aliona.binzari <alionab79@gmail.com>
2023-10-02 14:12:36 +0300

    Tema pentru acasa, ex.3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String sBoolean =  ("tRue");
+        boolean bVal = Boolean.parseBoolean(sBoolean);
+
+        String sByte = "120";
+        byte byVal = Byte.parseByte(sByte);
+
+        String sShort = "32000";
+        short sVal = Short.parseShort(sShort);
+
+        String sInt = "147895632";
+        int iVal = Integer.parseInt(sInt);
+
+        String sLong = "987654321987654321";
+        long lVal = Long.parseLong(sLong);
+        System.out.println(lVal);
+
+        String sDouble = "4444.5d";
+        double dVal = Double.parseDouble(sDouble);
+
+        String sFloat = "2354.56f";
+        float fVal = Float.parseFloat(sFloat);
+
+        System.out.println("boolean-"+bVal);
+        System.out.println("byte-"+byVal);
+        System.out.println("short-"+sVal);
+        System.out.println("int-"+iVal);
+        System.out.println("long-"+lVal);
+        System.out.println("double-"+dVal);
+        System.out.println("float -"+fVal);
     }
 }
+
